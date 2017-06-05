1f8191b2af85dd5906f00e47e623ab15d7dfd7d4
davidescus <rahthman_s@yahoo.com>
2017-06-05 22:31:29 +0300

    Commit to git some gibrish code

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
+import "fmt"
+
+type custom struct {
+	firstname
+	lastname
+}
+
+type custom1 struct {
+	firstname
+	lastname
+}
+
 func main() {
-   
-    fmt.Println("This is a simple text")
-   
-   // This is a modification from git hub
-   
+
+	fmt.Println("This is a simple text")
+	// This is a modification from git hub
+
+	custom.firstname = "Firstname String"
+	custom.lastname = "Custom lastname"
+
+	custom1.firstname = "This is the second struct of custom"
+
 }
